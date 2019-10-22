9b71737d9cd0b34651a564f1f158ee031758ff1f
Admin <anirudh7878@gmail.com>
2019-10-22 12:34:05 +0530

    .

---
+++ test.py
@@ -0,0 +1,10 @@
+users[str(username_info)] = {}
+        users[str(username_info)]["F_Name"] = ""
+        users[str(username_info)]["L_Name"] = ""
+        users[str(username_info)]["DOB"] = ""
+        users[str(username_info)]["Gender"] = ""
+        users[str(username_info)]["Email_ID"] = ""
+        users[str(username_info)]["ph_no"] = ""
+        users[str(username_info)]["10_percent"] = ""
+        users[str(username_info)]["12_percent"] = ""
+        users[str(username_info)]["Password"] = str(password_info)
