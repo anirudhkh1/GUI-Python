3770757d0322395eec11fdb908d031bc836e11c1
Admin <anirudh7878@gmail.com>
2019-11-23 10:41:30 +0530

    .

--- Encrypt Decrypt GUI/Encrypt_Decrypt_GUI.py
+++ Encrypt Decrypt GUI/Encrypt_Decrypt_GUI.py
@@ -1,42 +1,33 @@
 from tkinter import *
-
-# import other necessery modules
 import random
 import time
-import datetime
 
-# creating root object
 root = Tk()
 
-# defining size of window
 root.geometry("1200x6000")
 
-# setting up the title of window
 root.title("Message Encryption and Decryption")
 
-Tops = Frame(root, width = 1600, relief = SUNKEN)
-Tops.pack(side = TOP)
+Tops = Frame(root, width=1600, relief=SUNKEN)
+Tops.pack(side=TOP)
 
-f1 = Frame(root, width = 800, height = 700,
-                            relief = SUNKEN)
-f1.pack(side = LEFT)
+f1 = Frame(root, width=800, height=700,
+           relief=SUNKEN)
+f1.pack(side=LEFT)
 
-# ==============================================
-#                  TIME
-# ==============================================
 localtime = time.asctime(time.localtime(time.time()))
 
-lblInfo = Label(Tops, font = ('helvetica', 50, 'bold'),
-          text = "SECRET MESSAGING \n Vigenre cipher",
-                     fg = "Black", bd = 10, anchor='w')
+lblInfo = Label(Tops, font=('helvetica', 50, 'bold'),
+                text="SECRET MESSAGING \n Vigenre cipher",
+                fg="Black", bd=10, anchor='w')
 
-lblInfo.grid(row = 0, column = 0)
+lblInfo.grid(row=0, column=0)
 
 lblInfo = Label(Tops, font=('arial', 20, 'bold'),
-             text = localtime, fg = "Steel Blue",
-                           bd = 10, anchor = 'w')
+                text=localtime, fg="Steel Blue",
+                bd=10, anchor='w')
 
-lblInfo.grid(row = 1, column = 0)
+lblInfo.grid(row=1, column=0)
 
 rand = StringVar()
 Msg = StringVar()
@@ -44,11 +35,11 @@ key = StringVar()
 mode = StringVar()
 Result = StringVar()
 
-# exit function
+
 def qExit():
     root.destroy()
 
-# Function to reset the window
+
 def Reset():
     rand.set("")
     Msg.set("")
@@ -57,68 +48,65 @@ def Reset():
     Result.set("")
 
 
-# reference
-lblReference = Label(f1, font = ('arial', 16, 'bold'),
-                text = "Name:", bd = 16, anchor = "w")
+lblReference = Label(f1, font=('arial', 16, 'bold'),
+                     text="Name:", bd=16, anchor="w")
 
-lblReference.grid(row = 0, column = 0)
+lblReference.grid(row=0, column=0)
 
-txtReference = Entry(f1, font = ('arial', 16, 'bold'),
-               textvariable = rand, bd = 10, insertwidth = 4,
-                        bg = "powder blue", justify = 'right')
+txtReference = Entry(f1, font=('arial', 16, 'bold'),
+                     textvariable=rand, bd=10, insertwidth=4,
+                     bg="powder blue", justify='right')
 
-txtReference.grid(row = 0, column = 1)
+txtReference.grid(row=0, column=1)
 
-# labels
-lblMsg = Label(f1, font = ('arial', 16, 'bold'),
-         text = "MESSAGE", bd = 16, anchor = "w")
+lblMsg = Label(f1, font=('arial', 16, 'bold'),
+               text="MESSAGE", bd=16, anchor="w")
 
-lblMsg.grid(row = 1, column = 0)
+lblMsg.grid(row=1, column=0)
 
-txtMsg = Entry(f1, font = ('arial', 16, 'bold'),
-         textvariable = Msg, bd = 10, insertwidth = 4,
-                bg = "powder blue", justify = 'right')
+txtMsg = Entry(f1, font=('arial', 16, 'bold'),
+               textvariable=Msg, bd=10, insertwidth=4,
+               bg="powder blue", justify='right')
 
-txtMsg.grid(row = 1, column = 1)
+txtMsg.grid(row=1, column=1)
 
-lblkey = Label(f1, font = ('arial', 16, 'bold'),
-            text = "KEY", bd = 16, anchor = "w")
+lblkey = Label(f1, font=('arial', 16, 'bold'),
+               text="KEY", bd=16, anchor="w")
 
-lblkey.grid(row = 2, column = 0)
+lblkey.grid(row=2, column=0)
 
-txtkey = Entry(f1, font = ('arial', 16, 'bold'),
-         textvariable = key, bd = 10, insertwidth = 4,
-                bg = "powder blue", justify = 'right')
+txtkey = Entry(f1, font=('arial', 16, 'bold'),
+               textvariable=key, bd=10, insertwidth=4,
+               bg="powder blue", justify='right')
 
-txtkey.grid(row = 2, column = 1)
+txtkey.grid(row=2, column=1)
 
-lblmode = Label(f1, font = ('arial', 16, 'bold'),
-          text = "MODE(e for encrypt, d for decrypt)",
-                                bd = 16, anchor = "w")
+lblmode = Label(f1, font=('arial', 16, 'bold'),
+                text="MODE(e for encrypt, d for decrypt)",
+                bd=16, anchor="w")
 
-lblmode.grid(row = 3, column = 0)
+lblmode.grid(row=3, column=0)
 
-txtmode = Entry(f1, font = ('arial', 16, 'bold'),
-          textvariable = mode, bd = 10, insertwidth = 4,
-                  bg = "powder blue", justify = 'right')
+txtmode = Entry(f1, font=('arial', 16, 'bold'),
+                textvariable=mode, bd=10, insertwidth=4,
+                bg="powder blue", justify='right')
 
-txtmode.grid(row = 3, column = 1)
+txtmode.grid(row=3, column=1)
 
-lblService = Label(f1, font = ('arial', 16, 'bold'),
-             text = "The Result-", bd = 16, anchor = "w")
+lblService = Label(f1, font=('arial', 16, 'bold'),
+                   text="The Result-", bd=16, anchor="w")
 
-lblService.grid(row = 2, column = 2)
+lblService.grid(row=2, column=2)
 
-txtService = Entry(f1, font = ('arial', 16, 'bold'),
-             textvariable = Result, bd = 10, insertwidth = 4,
-                       bg = "powder blue", justify = 'right')
+txtService = Entry(f1, font=('arial', 16, 'bold'),
+                   textvariable=Result, bd=10, insertwidth=4,
+                   bg="powder blue", justify='right')
 
-txtService.grid(row = 2, column = 3)
+txtService.grid(row=2, column=3)
 
-# Vigen�re cipher
 import base64
 
-# Function to encode
+
 def encode(key, clear):
     enc = []
 
@@ -131,7 +119,7 @@ def encode(key, clear):
 
     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
-# Function to decode
+
 def decode(key, enc):
     dec = []
 
@@ -139,7 +127,7 @@ def decode(key, enc):
     for i in range(len(enc)):
         key_c = key[i % len(key)]
         dec_c = chr((256 + ord(enc[i]) -
-                           ord(key_c)) % 256)
+                     ord(key_c)) % 256)
 
         dec.append(dec_c)
     return "".join(dec)
@@ -157,23 +145,20 @@ def Ref():
     else:
         Result.set(decode(k, clear))
 
-# Show message button
-btnTotal = Button(f1, padx = 16, pady = 8, bd = 16, fg = "black",
-                        font = ('arial', 16, 'bold'), width = 10,
-                       text = "Show Message", bg = "powder blue",
-                         command = Ref).grid(row = 7, column = 1)
 
-# Reset button
-btnReset = Button(f1, padx = 16, pady = 8, bd = 16,
-                  fg = "black", font = ('arial', 16, 'bold'),
-                    width = 10, text = "Reset", bg = "green",
-                   command = Reset).grid(row = 7, column = 2)
+btnTotal = Button(f1, padx=16, pady=8, bd=16, fg="black",
+                  font=('arial', 16, 'bold'), width=10,
+                  text="Show Message", bg="powder blue",
+                  command=Ref).grid(row=7, column=1)
 
-# Exit button
-btnExit = Button(f1, padx = 16, pady = 8, bd = 16,
-                 fg = "black", font = ('arial', 16, 'bold'),
-                      width = 10, text = "Exit", bg = "red",
-                  command = qExit).grid(row = 7, column = 3)
+btnReset = Button(f1, padx=16, pady=8, bd=16,
+                  fg="black", font=('arial', 16, 'bold'),
+                  width=10, text="Reset", bg="green",
+                  command=Reset).grid(row=7, column=2)
 
-# keeps window alive
+btnExit = Button(f1, padx=16, pady=8, bd=16,
+                 fg="black", font=('arial', 16, 'bold'),
+                 width=10, text="Exit", bg="red",
+                 command=qExit).grid(row=7, column=3)
+
 root.mainloop()
