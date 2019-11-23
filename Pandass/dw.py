1daa6a8ba6ef92c99fb164c6acec97e69ebc9e20
Anirudh <anirudh7878@gmail.com>
2019-11-24 01:10:44 +0530

    .

--- Pandass/dw.py
+++ Pandass/dw.py
@@ -1,12 +1,5 @@
-import colorama
-
-# If using Windows, init() will cause anything sent to stdout or stderr
-# will have ANSI color codes converted to the Windows versions. Hooray!
-# If you are already using an ANSI compliant shell, it won't do anything
-
-
-# Now regular ANSI codes should work, even in Windows
-CLEAR_SCREEN = '\033[2J'
-RED = '\033[31m'   # mode 31 = red forground
-RESET = '\033[0m'  # mode 0  = reset
-print(CLEAR_SCREEN + RED + 'Welcome!' + RESET)
+d = {'1': {'Service': ['BathroomKit', 'RoomKit'],
+           'Food': ['PaneerTikka', 'ChickenTikka', 'LongIceland', 'BlueLagoon', 'PaneeMasala', 'ChickenMasala',
+                    'Veg.Kofta']}}
+d["1"]["Service"].remove("BathroomKit")
+print(d)
